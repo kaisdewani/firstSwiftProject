0884d6889e739da287920e5a20d6c5ac2c5060bc
Kais Dewani <kaisdewani@yahoo.com>
2019-04-21 13:47:01 -0400

    added update function for button on view

--- mveecee/Controller/ViewController.swift
+++ mveecee/Controller/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var detailedDescLabel: UILabel!
+    
+    
+    @IBAction func updateBtnWasPressed(_ sender: Any) {
+        
     }
-
-
+    
+    
 }
 
